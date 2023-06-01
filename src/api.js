e7e1eeac7a3e7693e4402dd609392ab2dfd1468f
sendaarriaga <132060763+sendaarriaga@users.noreply.github.com>
2023-06-01 13:38:49 +0200

    Finishing AleatoryQuote

--- src/api.js
+++ src/api.js
@@ -1,18 +1,11 @@
-// Asynchronous Function: a function that takes too long to execute and
-//   we don't want to wait to get the result back.
-//   An async function ALWAYS returns a Promise<...>
-//
-// async: mark a function that needs to call other asynchronous functions
-//
-
 export const loadQuotes = async () => {
-  const response = await fetch(`https://api.api-ninjas.com/v1/quotes?limit=1`,{ 
+  const response = await fetch(`https://api.api-ninjas.com/v1/quotes`,{ 
             headers: {
-              'x-api-key': 'pYcZQSGvKJZCghIo/2ORmA==WmsjHmK8RmvrLvAq'
+              'X-Api-Key': 'pYcZQSGvKJZCghIo/2ORmA==WmsjHmK8RmvrLvAq'
             }
           });
-  const quotes = await response.json();
-  return quotes;
+  const quotesload = await response.json();
+  return quotesload;
 };
 
 export const searchQuotes = async (category) => {
--- src/components/aleatory.jsx
+++ src/components/aleatory.jsx
@@ -1,26 +1,40 @@
-import React, { useEffect, useRef, useState } from "react";
-import { loadQuotes } from "../api";
+import React, { useEffect, useState } from "react";
+import { loadQuotes} from "../api";
 
 
-function AleatoryQuote (){
-    const [quotes, setQuotes] = useState(loadQuotes);
-    if (quotes === null) {
-      return <div>Loading...</div>;
-    }
-    if (quotes.length === 0) {
-      return <div>No quotes.</div>;
-    }
-    return (
-        <div className="quotes-list">
-          {quotes.map((quote) => (
-            <div className="quote">
-              <div className="title">{quote.quote}</div>
-              <div className="author">{quote.author}</div>
-              <div className="category">{quote.category}</div>
-            </div>
-          ))}
+const QuoteList = ({ quotes }) => {
+  
+  if (quotes === null) {
+    return <div>Loading...</div>;
+  }
+  if (quotes.length === 0) {
+    return <div>No quotes.</div>;
+  }
+  return (
+    <div className="quotes-list">
+      {quotes.map((quote) => (
+        <div className="quote">
+          <div className="title">{quote.quote}</div>
+          <div className="author">{quote.author}</div>
+          <div className="category">{quote.category}</div>
         </div>
-    )
+      ))}
+    </div>
+  );
+};
+
+function AleatoryQuote() {
+  const [quotes, setQuotes] = useState(null);
+
+  useEffect(() => {
+    loadQuotes().then((quotes) => setQuotes(quotes));
+  }, []);
+
+  return (
+    <div className="quote-aleatory">
+      <QuoteList quotes={quotes} />
+    </div>
+  );
 }
 
 export default AleatoryQuote;
